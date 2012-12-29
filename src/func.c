1e2307405500314893e647756ef2ddfe030e7853
Giuseppe Scalzi <giuseppe@osgate.org>
2012-12-29 02:38:54 +0100

    Fix something.

--- src/func.c
+++ src/func.c
@@ -15,12 +15,13 @@ void sighandler(int signum, siginfo_t *info, void *ptr){
  * Print the help
  */
 void help(){
-	printf("Usage: %s -i <interface> -e <protocol> -m <mode> -v <type> -c\n",APPNAME);
-	printf("-i\tthe network interface used to listen\n");
-	printf("-e\ta filter to select which packets sniff (currently available: tcp, udp, ip, icmp, arp).\n\tExample: \"tcp && port 80\"\n");
-	printf("-m\tif set to 0 enable promiscous mode (default), 1 to disable\n");
-	printf("-v\tview mode, if \"full\" is specified, more information about the packet will print.\n\tWorks only with some protocols. Default \"simple\".\n");
-	printf("-c\tcontrols if the IPv4 checksum is correct\n");
+	printf("Usage: %s -i <interface> -e <protocol> -m <mode> -v -c -n\n",APPNAME);
+	printf("-i\tThe network interface used to listen.\n");
+	printf("-e\tA filter to select which packets sniff (currently available: tcp, udp, ip, icmp, arp).\n\tExample: \"tcp && port 80\"\n");
+	printf("-m\tOptional\tIf set to 0 enable promiscous mode (default), 1 to disable.\n");
+	printf("-v\tOptional\tPrint more information about the packets.\n\t\t\tWorks only with some protocols.\n");
+	printf("-c\tOptional\tControls if the IPv4 checksum is correct.\n");
+	printf("-n\tOptional\tTry to resolve IP addresses to DNS names.\n");
 	exit(1);
 }
 
